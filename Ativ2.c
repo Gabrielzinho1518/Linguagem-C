69f299b1728567fbe18c6e050171278d0ebe5e05
Gabriel <167330424+Gabrielzinho1518@users.noreply.github.com>
2024-10-01 10:49:16 -0300

    Atividade sobre Ponteiros
    Ativ2

---
+++ Ativ2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int x[5] = {1, 2, 3, 4, 5};
+    int y[5]= {6, 7, 8, 9, 10};
+
+    soma = x + y;
+        printf("%d", x[i]);
+    {
+        printf("\n");
+        for(i=0; i<5; i++);
+            printf("%d", s;
+    }
+    return 0;
+}
