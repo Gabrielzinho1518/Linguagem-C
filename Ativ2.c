c147fb2b5f91c7c35e98f50e2b758be74d195dcb
Gabriel <167330424+Gabrielzinho1518@users.noreply.github.com>
2024-10-01 11:08:17 -0300

    Update Ativ2.c
    Ponteiros

--- Ativ2.c
+++ Ativ2.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 int main(){
     int x[5] = {1, 2, 3, 4, 5};
-    int y[5]= {6, 7, 8, 9, 10};
+    int y[5]= {10, 20, 30, 40, 50};
+    int soma [5], *px, *py, *psoma, i;
 
-    soma = x + y;
-        printf("%d", x[i]);
-    {
-        printf("\n");
-        for(i=0; i<5; i++);
-            printf("%d", s;
+    px = x; //px = &x[0];
+    py = y;
+    psoma = soma;
+
+    // somar 2 vetores.
+    for (i=0; i<5; i++){
+        //soma [i] = x[i] + y[i];
+        *(psoma+i) = *(px + i) + *(py+i);
+        printf("Soma[%d] = %d\n",i,*(soma+i));
+
     }
     return 0;
 }
